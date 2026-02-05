c1fbab8e9aa4644b9e75260690f3fdac8150eee2
Aitor Basarrate <basarrate.aitor@gmail.com>
2026-02-05 11:44:45 +0100

    build errors fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // @ts-expect-error vitest configuration
+
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
   },
-})
+});
